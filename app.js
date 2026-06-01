a77a8d2e32ad212d908fdf7b23d35dd11a843ab8
AOrnstein <ornstein.adam@gmail.com>
2026-06-01 09:59:50 -0600

    implement core api functionality

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import employeesRouter from "#api/employees";
 const app = express();
 export default app;
 
-// TODO: this file!
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fullstack Employees API.");
+});
+
+// Routers
+app.use("/employees", employeesRouter);
+
+// Catch-all error-handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).send("Sorry! Something went wrong :(");
+});
