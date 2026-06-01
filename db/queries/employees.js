85da8375f5a84eecc66bd08e13c693e1806043b8
AOrnstein <ornstein.adam@gmail.com>
2026-05-31 22:20:16 -0600

    Init DB impl and seeding

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,13 +1,31 @@
+import db from "#db/client";
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
+  const sql = `
+  INSERT INTO employees
+    (name, birthday, salary)
+  VALUES
+    ($1, $2, $3)
+  RETURNING *
+  `;
+  const values = [name, birthday, salary];
+  const {
+    rows: [employee],
+  } = await db.query(sql, values);
+  return employee;
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const sql = `
+  SELECT *
+  FROM employees
+  `;
+  const { rows: employees } = await db.query(sql);
+  return employees;
 }
 
 /**
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +8,13 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    console.log(
+      await createEmployee({
+        name: faker.person.fullName(),
+        birthday: faker.date.birthdate(),
+        salary: faker.number.int({ min: 40000, max: 200000 }),
+      }),
+    );
+  }
 }
